1f48629ed66c0f4790bf47a41c7499bee6b531af
Naga Anvesh <knanvesh.01@gmail.com>
2021-06-23 19:24:48 +0530

    Ui Part 1

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+import mealsImage from '../../Resources/meals.jpg';
+import classes from "./styles/Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header(props) {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1> A react App</h1>
+                <HeaderCartButton>Cart</HeaderCartButton>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="Meals Image"/>
+            </div>
+        </Fragment>
+    );
+}
+
+export default Header;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CartIcon from "../Cart/CartIcon";
+import classes from "./styles/HeaderCartButton.module.css";
+
+function HeaderCartButton(props) {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon/>
+            </span>
+            <span>Cart</span>
+            <span className={classes.badge}>3</span>
+        </button>
+    );
+}
+
+export default HeaderCartButton;
